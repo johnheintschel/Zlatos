23f5b8940791037b1fff5d74fdba9d7595c91be1
John Heintschel <jch2196@columbia.edu>
2015-09-20 21:11:49 -0400

    Added mongoid

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,8 @@ module Zlatos
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-  end
+        config.generators do |g|
+            g.orm :mongoid  
+        end
+    end
 end

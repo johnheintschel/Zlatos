80ef597de0a9c67ed8b28a94a7b9251ff8ad689b
John Heintschel <jch2196@columbia.edu>
2015-09-22 21:34:34 -0400

    fixed Gemfile

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email])
     if user
-      session[:user_id] = user.id 
+      session[:user_id] = user.id
+      @session =  
 
 
       redirect_to root_url
